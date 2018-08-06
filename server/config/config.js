dfd4687d2699624443b2e7c5c1616fba97d99ad9
pablodelacuadra <pablo.delacuadra@outlook.com>
2018-08-06 02:13:13 -0400

    Variable de Entorno Mongo

--- server/config/config.js
+++ server/config/config.js
@@ -10,10 +10,10 @@ process.env.NODE_ENV = process.env.NODE_ENV || 'dev';
 
 let urlDB;
 
-if (!process.env.NODE_ENV === 'dev') {
+if (process.env.NODE_ENV === 'dev') {
     urlDB = 'mongodb://localhost:27017/cafe';
 } else {
-    urlDB = 'mongodb://cafe-user:123456a@ds113732.mlab.com:13732/cafe';
+    urlDB = process.env.MONGO_URL;
 }
 
 process.env.URLDB = urlDB;
